3398402dbb162dc43e780186895f706ce86e5c57
Rajnish <rajnishlather7@gmail.com>
2026-07-04 01:41:50 +0200

    re trigger

--- docs/config.js
+++ docs/config.js
@@ -1,3 +1,3 @@
 // staticgames.staticgames.workers.dev 
 // partykit dev --command "bun run wrangler.js" 
-export const PARTYKIT_HOST = "staticgames-2.staticgames.workers.dev";
+export const PARTYKIT_HOST = "staticgames.staticgames.workers.dev";
